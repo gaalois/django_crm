a928c9a9b21a2a8d8fc7d101632ee6830e3c013d
gaalois <rograndrianirina@gmail.com>
2025-01-26 18:24:37 +0900

    registration

--- mim_app/views.py
+++ mim_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # Check if the user is logging in
+    if request.method == 'POST':
+        username = request.POST.get('username')  # Utilisez .get() pour éviter KeyError
+        password = request.POST.get('password')
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in successfully.")
+            return redirect('home')  # Redirection après connexion réussie
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+            return redirect('home')  # Redirection après échec
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)  # Déconnexion de l'utilisateur
+    messages.success(request, "You have been logged out successfully.")
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
